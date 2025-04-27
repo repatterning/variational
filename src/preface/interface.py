745e4e4a770b1d8b8a2300029a2015948d95506b
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-04-27 23:51:25 +0100

    Add src/preface/interface.py

---
+++ src/preface/interface.py
@@ -0,0 +1,49 @@
+"""Module interface.py"""
+import typing
+
+import boto3
+
+import src.elements.s3_parameters as s3p
+import src.elements.service as sr
+import src.functions.service
+import src.preface.setup
+import src.s3.configurations
+import src.s3.s3_parameters
+
+
+class Interface:
+    """
+    Interface
+    """
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def __get_arguments(connector: boto3.session.Session) -> dict:
+        """
+
+        :return:
+        """
+
+        key_name = 'artefacts' + '/' + 'architecture' + '/' + 'arguments.json'
+
+        arguments = src.s3.configurations.Configurations(connector=connector).objects(key_name=key_name)
+
+        return arguments
+
+    def exc(self) -> typing.Tuple[boto3.session.Session, s3p.S3Parameters, sr.Service, dict]:
+        """
+
+        :return:
+        """
+
+        connector = boto3.session.Session()
+        s3_parameters: s3p.S3Parameters = src.s3.s3_parameters.S3Parameters(connector=connector).exc()
+        service: sr.Service = src.functions.service.Service(
+            connector=connector, region_name=s3_parameters.region_name).exc()
+        arguments: dict = self.__get_arguments(connector=connector)
+
+        src.preface.setup.Setup(service=service, s3_parameters=s3_parameters).exc()
+
+        return connector, s3_parameters, service, arguments
