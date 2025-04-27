26aee49b755fa15245f5180cccfd00ad6a1f0374
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-04-27 23:56:17 +0100

    Add src/main.py

---
+++ src/main.py
@@ -0,0 +1,52 @@
+"""Module main.py"""
+import datetime
+import logging
+import os
+import sys
+import boto3
+
+
+def main():
+
+    """
+    Entry Point
+
+    :return:
+    """
+
+    logger: logging.Logger = logging.getLogger(__name__)
+    logger.info('Starting: %s', datetime.datetime.now().isoformat(timespec='microseconds'))
+
+    partitions = src.assets.interface.Interface(
+        service=service, s3_parameters=s3_parameters, arguments=arguments).exc()
+    logger.info(partitions)
+
+    # Deleting __pycache__
+    src.functions.cache.Cache().exc()
+
+if __name__ == '__main__':
+
+    root = os.getcwd()
+    sys.path.append(root)
+    sys.path.append(os.path.join(root, 'src'))
+
+    # Logging
+    logging.basicConfig(level=logging.INFO,
+                        format='\n\n%(message)s\n%(asctime)s.%(msecs)03d\n',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+
+    # Modules
+    import src.assets.interface
+    import src.elements.s3_parameters as s3p
+    import src.elements.service as sr
+    import src.functions.cache
+    import src.preface.interface
+
+    connector: boto3.session.Session
+    s3_parameters: s3p.S3Parameters
+    service: sr.Service
+    arguments: dict
+    connector, s3_parameters, service, arguments = src.preface.interface.Interface().exc()
+
+    main()
+
