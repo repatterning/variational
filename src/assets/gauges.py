e08b844258b5cfda5c1c1d9ff3a33b5bb6a9568f
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-05-07 23:21:25 +0100

    Update src/assets/gauges.py

--- src/assets/gauges.py
+++ src/assets/gauges.py
@@ -12,21 +12,25 @@ class Gauges:
     Retrieves the catchment & time series codes of the gauges in focus.
     """
 
-    def __init__(self, service: sr.Service, s3_parameters: s3p.S3Parameters):
+    def __init__(self, service: sr.Service, s3_parameters: s3p.S3Parameters, arguments: dict):
         """
 
         :param service:
         :param s3_parameters:
+        :param arguments:
         """
 
         self.__service = service
         self.__s3_parameters = s3_parameters
+        self.__arguments = arguments
 
         # An instance for interacting with objects within an Amazon S3 prefix
-        self.__pre = src.s3.prefix.Prefix(service=self.__service, bucket_name=self.__s3_parameters.internal)
+        self.__pre = src.s3.prefix.Prefix(
+            service=self.__service,
+            bucket_name=self.__s3_parameters._asdict()[arguments['s3']['p_bucket']])
 
     @staticmethod
-    def __get_elements(objects: list) -> pd.DataFrame:
+    def __get_elements(objects: list[str]) -> pd.DataFrame:
         """
 
         :param objects:
@@ -40,11 +44,33 @@ class Gauges:
         rename = {0: 'endpoint', 1: 'catchment_id', 2: 'ts_id', 3: 'name'}
         splittings = values['uri'].str.rsplit('/', n=3, expand=True)
         splittings.rename(columns=rename, inplace=True)
+        splittings['date'] = splittings['name'].str.replace('.csv', '')
 
         # Collating
         values = values.copy().join(splittings, how='left')
 
         return values
+
+    def __get_keys(self) -> list[str]:
+        """
+        cf. self.__pre.objects(prefix=paths[0], delimiter=''),
+        self.__pre.objects(prefix=paths[0], delimiter='/')
+
+        :return:
+        """
+
+        paths = self.__pre.objects(
+            prefix=(self.__s3_parameters._asdict()[self.__arguments['s3']['p_prefix']]
+                    + f"{self.__arguments['s3']['affix']}/"),
+            delimiter='/')
+
+        computations = []
+        for path in paths:
+            listings = self.__pre.objects(prefix=path, delimiter='')
+            computations.append(listings)
+        keys: list[str] = sum(computations, [])
+
+        return keys
 
     def exc(self) -> pd.DataFrame:
         """
@@ -52,7 +78,7 @@ class Gauges:
         :return:
         """
 
-        keys: list[str] = self.__pre.objects(prefix=self.__s3_parameters.path_internal_data + 'resamples')
+        keys = self.__get_keys()
         if len(keys) > 0:
             objects = [f's3://{self.__s3_parameters.internal}/{key}' for key in keys]
         else:
@@ -65,7 +91,7 @@ class Gauges:
         # Types
         values['catchment_id'] = values['catchment_id'].astype(dtype=np.int64)
         values['ts_id'] = values['ts_id'].astype(dtype=np.int64)
-        values.loc[:, 'datestr'] = values['name'].str.replace(pat='.csv', repl='')
+        values['date'] = pd.to_datetime(values['date'], format='%Y-%m-%d')
         values.drop(columns=['endpoint', 'name'], inplace=True)
 
         return values
