6c50b1a275e53e9e067e28628a282d6f0e0cf6a8
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-05-07 23:22:53 +0100

    Update src/elements/partitions.py

--- src/elements/partitions.py
+++ src/elements/partitions.py
@@ -8,17 +8,11 @@ class Partitions(typing.NamedTuple):
 
     Attributes<br>
     ----------<br>
-    <b>ts_id</b>: int<br>
-        The identification code of a time series.<br><br>
     <b>catchment_id</b>: int<br>
         The identification code of a catchment area.<br><br>
-    <b>datestr</b>: str<br>
-        The <b>date string of the start date of a period</b>, format %Y-%m-%d,  i.e., YYYY-mm-dd.<br><br>
-    <b>uri</b>: str<br>
-        A uniform resource locator
+    <b>ts_id</b>: int<br>
+        The identification code of a time series.<br><br>
     """
 
-    ts_id: int
     catchment_id: int
-    datestr: str
-    uri: str
+    ts_id: int
