a6c2e33c1b94d545b1967d5b820451229242ff43
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-05-08 01:37:51 +0100

    Update src/modelling/architecture.py

--- src/modelling/architecture.py
+++ src/modelling/architecture.py
@@ -1,8 +1,25 @@
+import tensorflow_probability as tfp
+
+import numpy as np
+
 
 class Architecture:
 
-    def __init__(self):
-        pass
+    def __init__(self, arguments: dict):
 
-    def __design(self):
-        pass
+        self.__arguments = arguments
+
+    def __design(self, _training: np.ndarray):
+
+        month_of_year_effect = tfp.sts.Seasonal(
+            num_seasons=self.__arguments.get('seasons').get('number_of'),
+            num_steps_per_season=self.__arguments.get('seasons').get('steps_per'),
+            observed_time_series=_training,
+            name='month_of_year_effect')
+
+        autoregressive = tfp.sts.Autoregressive(order=1, observed_time_series=_training, name='autoregressive')
+
+        model = tfp.sts.Sum([month_of_year_effect, autoregressive],
+                            observed_time_series=_training)
+
+        return model
