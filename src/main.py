2d096ce6540464ec4d4cd60173150b5dfa8b8906
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-05-08 17:47:21 +0100

    Update src/main.py

--- src/main.py
+++ src/main.py
@@ -34,6 +34,8 @@ if __name__ == '__main__':
     sys.path.append(root)
     sys.path.append(os.path.join(root, 'src'))
 
+    os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+
     # Logging
     logging.basicConfig(level=logging.INFO,
                         format='\n\n%(message)s\n%(asctime)s.%(msecs)03d\n',
