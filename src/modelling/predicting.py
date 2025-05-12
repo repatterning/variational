ad24ace481484829227d0e3c00f746182346061b
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-05-12 23:56:27 +0100

    Update src/modelling/predicting.py

--- src/modelling/predicting.py
+++ src/modelling/predicting.py
@@ -78,7 +78,7 @@ class Predicting:
 
         # Hence
         estimates = self.__estimates(data=data, times=times, p_distribution_samples=p_distribution_samples)
-        estimates['milliseconds'] = estimates['datetime'].astype(np.int64) / 1000
+        estimates['timestamp'] = estimates['datetime'].astype(np.int64) / 1000
         estimates.drop(columns='datetime', inplace=True)
 
         return estimates
